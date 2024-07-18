5fd40c4974d51de6b5fd0ada391f685ca9e39356
Byzgaev <110855219+Byzgaev-I@users.noreply.github.com>
2024-07-18 17:50:08 +0300

    Update providers.tf

--- src/providers.tf
+++ src/providers.tf
@@ -8,7 +8,6 @@ terraform {
 }
 
 provider "yandex" {
-  version = "~> 0.71"
   token     = "t1.9euelZqZksiSxsrLj5udmYyYlZqbmO3rnpWanIyRipSTnJOajZXHnJiPzYzl8_cNNRtL-e9fWDEr_N3z901jGEv5719YMSv8zef1656VmsjOmp6Xz8aVy5aRzI6Zk4mb7_zF656VmsjOmp6Xz8aVy5aRzI6Zk4mb.DDzmBHktbPXHpzUzynJxBf8okmHIaRdh1w8sBPPEhm2f1tHJXM1PpP-JL60LxapY9eAmI7Fzkl0lZqhA-1GKDQ"
   cloud_id  = "b1g31ab21b32dog1ps4c"  # Замените на ваш реальный cloud_id
   folder_id = "b1gam4o6rj97es4peaq4"  # Замените на ваш реальный folder_id
