cee033e286e736847d4379777bc3f2760c7133f0
Byzgaev <110855219+Byzgaev-I@users.noreply.github.com>
2024-07-18 16:28:29 +0300

    Update main.tf

--- src/main.tf
+++ src/main.tf
@@ -1,45 +1,24 @@
-terraform {
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "0.124.0"
-    }
-  }
-  required_version = ">=1.5"
-}
-
-provider "yandex" {
-  token     = var.token
-  cloud_id  = var.cloud_id
-  folder_id = var.folder_id
-  zone      = var.default_zone
-}
-
 resource "yandex_vpc_network" "develop" {
-  name      = var.vpc_name
-  folder_id = var.folder_id  # Добавили folder_id
+  name = var.vpc_name
 }
-
 resource "yandex_vpc_subnet" "develop" {
   name           = var.vpc_name
   zone           = var.default_zone
   network_id     = yandex_vpc_network.develop.id
   v4_cidr_blocks = var.default_cidr
-  folder_id      = var.folder_id  # Добавили folder_id
 }
+
 
 data "yandex_compute_image" "ubuntu" {
   family = "ubuntu-2004-lts"
 }
-
 resource "yandex_compute_instance" "platform" {
-  name        = local.web_name
-  platform_id = var.vm_web_platform
-  metadata    = var.vm_metadata
+  name        = "netology-develop-platform-web"
+  platform_id = "standart-v4"
   resources {
-    cores         = var.vm_resources.web.cores
-    memory        = var.vm_resources.web.memory
-    core_fraction = var.vm_resources.web.core_fraction
+    cores         = 1
+    memory        = 1
+    core_fraction = 5
   }
   boot_disk {
     initialize_params {
@@ -47,35 +26,16 @@ resource "yandex_compute_instance" "platform" {
     }
   }
   scheduling_policy {
-    preemptible = var.vm_web_policy_preemptible
+    preemptible = true
   }
   network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
-    nat       = var.vm_web_network_nat
+    nat       = true
   }
-  folder_id = var.folder_id  # Добавили folder_id
-}
 
-resource "yandex_compute_instance" "platform-db" {
-  name        = local.db_name
-  platform_id = var.vm_db_platform
-  metadata    = var.vm_metadata
-  resources {
-    cores         = var.vm_resources.db.cores
-    memory        = var.vm_resources.db.memory
-    core_fraction = var.vm_resources.db.core_fraction
+  metadata = {
+    serial-port-enable = 1
+    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
   }
-  boot_disk {
-    initialize_params {
-      image_id = data.yandex_compute_image.ubuntu.image_id
-    }
-  }
-  scheduling_policy {
-    preemptible = var.vm_db_policy_preemptible
-  }
-  network_interface {
-    subnet_id = yandex_vpc_subnet.develop.id
-    nat       = var.vm_db_network_nat
-  }
-  folder_id = var.folder_id  # Добавили folder_id
+
 }
