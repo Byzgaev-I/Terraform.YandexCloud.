535e45a997ace884b4eb5b8aa77d78e60076ffe3
Byzgaev <110855219+Byzgaev-I@users.noreply.github.com>
2024-07-18 16:20:03 +0300

    Update providers.tf

--- src/providers.tf
+++ src/providers.tf
@@ -1,4 +1,4 @@
-terraform {
+#terraform {
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
@@ -7,7 +7,7 @@ terraform {
   required_version = ">=0.13"
 }
 
-provider "yandex" {
+#provider "yandex" {
   token     = var.token
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
