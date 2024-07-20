9ccbae7e08ed29a871aa5eac994d1af5f4177749
Byzgaev <110855219+Byzgaev-I@users.noreply.github.com>
2024-07-20 14:38:48 +0300

    Update main.tf

--- src/main.tf
+++ src/main.tf
@@ -1,7 +1,6 @@
 resource "yandex_vpc_network" "develop" {
   name = var.vpc_name
 }
-
 resource "yandex_vpc_subnet" "develop" {
   name           = var.vpc_name
   zone           = var.default_zone
@@ -10,16 +9,44 @@ resource "yandex_vpc_subnet" "develop" {
 }
 
 data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu-2004-lts"
+  family = var.vm_web_family
+}
+resource "yandex_compute_instance" "platform1" {
+  name =  local.vm_web_instance_name
+  platform_id = var.vm_web_platform_id
+  metadata = var.common_metadata
+  resources {
+    cores         = var.vms_resources.vm_web_resources.cores
+    memory        = var.vms_resources.vm_web_resources.memory
+    core_fraction = var.vms_resources.vm_web_resources.core_fraction
+  }
+
+ boot_disk {
+      initialize_params {
+        image_id = data.yandex_compute_image.ubuntu.image_id
+      }
+    }
+   scheduling_policy {
+      preemptible = true
+    }
+    network_interface {
+      subnet_id = yandex_vpc_subnet.develop.id
+      nat       = true
+   }
 }
 
-resource "yandex_compute_instance" "platform" {
-  name        = "netology-develop-platform-web"
-  platform_id = "standard-v1"
+
+data "yandex_compute_image" "ubuntu2" {
+  family = var.vm_db_family
+}
+resource "yandex_compute_instance" "platform2" {
+  name        = local.vm_db_instance_name
+  platform_id = var.vm_db_platform_id
+  metadata    = var.common_metadata
   resources {
-    cores         = 1
-    memory        = 1
-    core_fraction = 5
+    cores         = var.vms_resources.vm_db_resources.cores
+    memory        = var.vms_resources.vm_db_resources.memory
+    core_fraction = var.vms_resources.vm_db_resources.core_fraction
   }
   boot_disk {
     initialize_params {
@@ -33,17 +60,4 @@ resource "yandex_compute_instance" "platform" {
     subnet_id = yandex_vpc_subnet.develop.id
     nat       = true
   }
-
-  metadata = {
-    serial-port-enable = "1"
-    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
-  }
-}
-
-output "instance_id" {
-  value = yandex_compute_instance.platform.id
-}
-
-output "external_ip_address" {
-  value = yandex_compute_instance.platform.network_interface[0].nat_ip_address
 }
