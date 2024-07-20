92313157bbcf18ef45fd348080cd077655877668
Byzgaev <110855219+Byzgaev-I@users.noreply.github.com>
2024-07-20 12:34:38 +0300

    Delete src/locals.tf

--- src/locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  project = "netology-develop-platform"
-  env_web = "web"
-  env_db  = "db"
-  vm_web_instance_name = "${local.project}-${local.env_web}"
-  vm_db_instance_name = "${local.project}-${local.env_db}"
-}
