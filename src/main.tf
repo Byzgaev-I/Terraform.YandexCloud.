629041411602a208e62b8a21ad9476e567489bfd
Byzgaev <110855219+Byzgaev-I@users.noreply.github.com>
2024-07-20 14:44:42 +0300

    Update main.tf

--- src/main.tf
+++ src/main.tf
@@ -1,7 +1,4 @@
-resource "yandex_vpc_network" "develop" {
-  name = var.vpc_name
-}
-resource "yandex_vpc_subnet" "develop" {
+source "yandex_vpc_subnet" "develop" {
   name           = var.vpc_name
   zone           = var.default_zone
   network_id     = yandex_vpc_network.develop.id
@@ -9,28 +6,40 @@ resource "yandex_vpc_subnet" "develop" {
 }
 
 data "yandex_compute_image" "ubuntu" {
-  family = var.vm_web_family
+  family = "ubuntu-2004-lts"
 }
-resource "yandex_compute_instance" "platform1" {
-  name =  local.vm_web_instance_name
-  platform_id = var.vm_web_platform_id
-  metadata = var.common_metadata
+
+resource "yandex_compute_instance" "platform" {
+  name        = "netology-develop-platform-web"
+  platform_id = "standard-v4"
   resources {
-    cores         = var.vms_resources.vm_web_resources.cores
-    memory        = var.vms_resources.vm_web_resources.memory
-    core_fraction = var.vms_resources.vm_web_resources.core_fraction
+    cores         = 1
+    memory        = 1
+    core_fraction = 5
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
   }
 
- boot_disk {
-      initialize_params {
-        image_id = data.yandex_compute_image.ubuntu.image_id
-      }
-    }
-   scheduling_policy {
-      preemptible = true
-    }
-    network_interface {
-      subnet_id = yandex_vpc_subnet.develop.id
-      nat       = true
-   }
+  metadata = {
+    serial-port-enable = "1"
+    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+  }
+}
+
+output "instance_id" {
+  value = yandex_compute_instance.platform.id
+}
+
+output "external_ip_address" {
+  value = yandex_compute_instance.platform.network_interface[0].nat_ip_address
 }
